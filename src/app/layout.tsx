42756fd5c693f7a36b18e94157d44eded271fc7a
lucasdwn <lfcosta0804@gmail.com>
2024-09-27 01:26:23 -0300

    👷 Feat: Settings to deploy.
    Implementando configurações e correções para o deploy.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from "@/components/theme/themeProvider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AuthProvider } from "@/context/authContext";
 
-
 export const metadata: Metadata = {
   title: "Tarefas",
   description: "Gerir tarefas",
@@ -16,15 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
-      </head>
-      <body
-        className={`antialiased`}
-      >
+    <html lang="pt-BR">
+      <body className="antialiased">
         <ThemeProvider>
           <AuthProvider>
             <TooltipProvider>
