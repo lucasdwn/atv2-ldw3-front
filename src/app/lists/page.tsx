04152ea669d7b611abbaf12fce2a7581f32cb0c1
lucasdwn <lfcosta0804@gmail.com>
2024-09-27 23:10:48 -0300

    ♻️ Adicionando header e main em lists
    Refatorando codigo da pagina de lists, adicionando header e main.

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -5,14 +5,19 @@ import ProtectedLayout from "../protectedLayout";
 export default function HomePage() {
     return (
         <ProtectedLayout>
-            <h1 className="text-4xl font-bold mb-8">Suas listas</h1>
-            <div className="flex flex-col items-center justify-center h-[calc(100vh-200px)]">
-                <p className="text-xl mb-4 text-center">Crie já sua primeira lista.</p>
-                <Button>
-                    <ListPlus className="mr-2" />
-                    Nova lista
-                </Button>
-            </div>
+            <header>
+                <h1 className="text-4xl font-bold mb-8">Suas listas</h1>
+            </header>
+            <main>
+
+                <div className="flex flex-col items-center justify-center h-[calc(100vh-200px)]">
+                    <p className="text-xl mb-4 text-center">Crie já sua primeira lista.</p>
+                    <Button>
+                        <ListPlus className="mr-2" />
+                        Nova lista
+                    </Button>
+                </div>
+            </main>
         </ProtectedLayout>
     );
 }
