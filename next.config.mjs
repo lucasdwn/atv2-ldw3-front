0d3c1486e3af0520db07bd474e9df7059676544b
lucasdwn <lfcosta0804@gmail.com>
2024-09-29 17:12:04 -0300

    🔧 Atualizando método de conexão externa.
    Corrigindo bug onde image.domains era depreciada e atualizando para novo uso 'image.remotePatterns'

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['imgur.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'imgur.com',
+            },
+        ],
     },
 };
 
